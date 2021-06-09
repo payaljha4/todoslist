63b9cddcfcf40827c0ef36e63faecc8d013833f0
Payal <payaljha.24200@gmail.com>
2021-06-10 00:08:44 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header';
+import {Footer} from './Component/Footer';
+import {Todos} from './Component/Todos';
+import React, { useState, useEffect } from 'react';
+import { AddTodo } from './Component/AddTodo';
+import { About } from './Component/About';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
 function App() {
+  let initTodo;
+  if(localStorage.getItem("todos")){
+    initTodo=[];
+  }
+  else{
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const onDelete=(todo)=>{
+    console.log("I am ondelete of todo", todo)
+    setTodos(todos.filter((e)=>{
+      return e!==todo;
+    }));
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+  const addTodo=(title, desc)=>{
+    console.log("I am adding this todo", title, desc);
+    let sno;
+    if(todos.length===0){
+      sno=0;
+    }
+    else{
+      sno = todos[todos.length-1].sno+1;
+    }
+    const myTodo={
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+    
+    
+    localStorage.setItem("todos", JSON.stringify(todos));
+    
+  }
+  const [todos, setTodos]=useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Header title="My Todos List" searchBar={false}/>
+    <Switch>
+          <Route exact path="/" render={()=>{
+            return(
+            <>
+            <AddTodo addTodo={addTodo}/>
+    <Todos todos={todos} onDelete={onDelete}/>
+    </>)
+          }}>
+            
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+    
+    <Footer/>
+    </Router>
+    </>
   );
 }
 
---
+++ src/Component/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about Component
+            <p>Excepteur ex aliquip dolor consequat cupidatat Lorem cillum commodo id consequat ullamco deserunt. Enim nisi mollit culpa velit cillum cupidatat mollit pariatur velit sit. Nostrud ad irure id ipsum ex exercitation. Elit ut reprehenderit minim ipsum reprehenderit est ea culpa elit minim consectetur nisi est esse. Qui minim aute ad irure ex duis minim non est dolore.</p>
+        </div>
+    )
+}
